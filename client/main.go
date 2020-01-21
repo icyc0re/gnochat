3c75414862db112449ff956fee1d917ddc00b110
Tiziano Signo <tizianosigno@peterssoftware.de>
2020-01-21 14:46:39 +0100

    add disconnect logic, CloseMessage handshake with server with timeout

--- client/main.go
+++ client/main.go
@@ -10,6 +10,10 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+const (
+	DISCONNECT_TIMEOUT = time.Second * 2
+)
+
 var (
 	host = flag.String("host", "localhost", "server hostname")
 	port = flag.String("port", "5003", "port on which server listens to")
@@ -29,6 +33,34 @@ func connect() *websocket.Conn {
 	return conn
 }
 
+func disconnect(conn *websocket.Conn) {
+	done := make(chan bool)
+
+	go func() {
+		err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "bye!"))
+		if err != nil {
+			fmt.Println("ERROR close:", err)
+			done <- false
+			return
+		}
+
+		// wait for close message
+		for {
+			_, _, err := conn.ReadMessage()
+			if err != nil && websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+				fmt.Println("Connection closed safely!")
+				done <- true
+				return
+			}
+		}
+	} ()
+
+	select {
+	case <-done:
+	case <-time.After(DISCONNECT_TIMEOUT):
+	}
+}
+
 func main() {
 	flag.Parse()
 
@@ -44,4 +76,7 @@ func main() {
 
 		time.Sleep(time.Millisecond * 100)
 	}
+
+	// disconnect
+	disconnect(conn)
 }
