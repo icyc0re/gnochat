573d4ca212f12a1f507935179016e4bfa8416767
Tiziano Signo <tizianosigno@peterssoftware.de>
2020-01-21 11:47:37 +0100

    add connection handler that prints remote address

--- server/main.go
+++ server/main.go
@@ -12,10 +12,16 @@ var (
 	port = flag.String("port", "5003", "port on which server listens to")
 )
 
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	log.Printf("New connection: %s", r.RemoteAddr)
+}
+
 func main() {
 	// start server
 	flag.Parse()
 	log.SetFlags(0)
+
+	http.HandleFunc("/echo", handleConnections)
 
 	addr := fmt.Sprintf("%s:%s", *host, *port)
 	log.Printf("Listening on %s", addr)
