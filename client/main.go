9b6d68deffded3697ba55afe1e7c226a9a2f16f9
Tiziano Signo <ts@tsmac.fritz.box>
2020-01-22 02:11:09 +0100

    add tview ui to the client

--- client/main.go
+++ client/main.go
@@ -3,80 +3,116 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net/url"
 	"os"
+	"strings"
 	"time"
 
-	"github.com/gorilla/websocket"
+	"github.com/gdamore/tcell"
+	"github.com/rivo/tview"
 )
 
 const (
 	DISCONNECT_TIMEOUT = time.Second * 2
+	COLOR_CURRENT_USER = "[#00ffff]"
+	COLOR_OTHER_USERS = "[green]"
 )
 
 var (
 	host = flag.String("host", "localhost", "server hostname")
 	port = flag.String("port", "5003", "port on which server listens to")
+	username = "user0"
 )
 
-func connect() *websocket.Conn {
-	addr := fmt.Sprintf("%s:%s", *host, *port)
-	u := url.URL{Scheme: "ws", Host: addr, Path: "/echo"}
-	fmt.Printf("connecting to %s\n", u.String())
+func viewUpdater(ch chan string, msgView *tview.TextView, app *tview.Application) {
+	for {
+		msg := <-ch
 
-	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	if err != nil {
-		fmt.Println("ERROR dial:",err)
-		os.Exit(1)
+		pos := strings.Index(msg, ":")
+		sender := msg[:pos]
+		text := msg[pos+1:]
+
+		// if message from current user:
+		if sender == username {
+			msgView.Write([]byte(COLOR_CURRENT_USER))
+		} else {
+			msgView.Write([]byte(COLOR_OTHER_USERS))
+		}
+		msgView.Write([]byte(tview.Escape(fmt.Sprintf("[%s] ", sender))))
+		msgView.Write([]byte("[white]"))
+		msgView.Write([]byte(tview.Escape(text)))
+		msgView.Write([]byte("\n"))
+		app.Draw()
 	}
-
-	return conn
 }
 
-func disconnect(conn *websocket.Conn) {
-	done := make(chan bool)
+func isEmptyString(s string) bool {
+	return len(strings.Trim(s, " ")) == 0
+}
 
-	go func() {
-		err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "bye!"))
-		if err != nil {
-			fmt.Println("ERROR close:", err)
-			done <- false
-			return
-		}
-
-		// wait for close message
-		for {
-			_, _, err := conn.ReadMessage()
-			if err != nil && websocket.IsCloseError(err, websocket.CloseNormalClosure) {
-				fmt.Println("Connection closed safely!")
-				done <- true
-				return
-			}
-		}
-	} ()
-
-	select {
-	case <-done:
-	case <-time.After(DISCONNECT_TIMEOUT):
-	}
+func allButColon(text string, ch rune) bool {
+	return ch != ':'
 }
 
 func main() {
 	flag.Parse()
 
-	conn := connect()
+	app := tview.NewApplication()
+
+	// username view
+	prompt := tview.NewInputField().
+		SetLabel("USERNAME: ").
+		SetAcceptanceFunc(allButColon)
+	prompt.SetDoneFunc(func (key tcell.Key) {
+			if text := prompt.GetText(); key == tcell.KeyEnter && !isEmptyString(text) {
+				username = prompt.GetText()
+				app.Stop()
+			}
+		})
+
+	if err := app.SetRoot(prompt, true).Run(); err != nil {
+		panic(err)
+	}
+
+	// connect
+	conn := Connect(*host, *port)
+	if conn == nil {
+		os.Exit(1)
+	}
+
 	defer conn.Close()
 
-	for i := 0; i < 20; i++ {
-		msg := []byte(fmt.Sprintf("msg %d", i))
-		err := conn.WriteMessage(websocket.TextMessage, msg)
-		if err != nil {
-			fmt.Println("ERROR write:", err)
-		}
+	outChan := make(chan string)
+	inChan := make(chan string)
 
-		time.Sleep(time.Millisecond * 100)
+	go MessageSender(conn, outChan)
+	go MessageReceiver(conn, inChan)
+
+	outChan <- username
+
+	mainView := tview.NewTextView().
+		SetDynamicColors(true)
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(mainView, 0, 1, false).
+		AddItem(prompt, 1, 1, true)
+	prompt.SetText("").SetLabel("> ").
+		SetDoneFunc(func (key tcell.Key) {
+			if text := prompt.GetText(); key == tcell.KeyEnter && !isEmptyString(text) {
+				if text == ":quit" {
+					app.Stop()
+				} else {
+					outChan <- text
+					prompt.SetText("")
+				}
+			}
+		})
+
+	go viewUpdater(inChan, mainView, app)
+
+	if err := app.SetRoot(flex, true).Run(); err != nil {
+		panic(err)
 	}
 
 	// disconnect
-	disconnect(conn)
+	Disconnect(conn)
 }
