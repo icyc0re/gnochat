7a5dc8150e96adb68a440ef4a541655814f8bdf1
Tiziano Signo <tizianosigno@peterssoftware.de>
2020-01-22 18:30:22 +0100

    update entry view of the chat with a simple username form (functionality not yet implemented)

---
+++ client_svelte/src/JoinForm.svelte
@@ -0,0 +1,20 @@
+<script>
+    let username = '';
+
+    function joinChat() {
+        console.log(username)
+        // TODO send message to the server
+    }
+</script>
+
+<form on:submit|preventDefault={joinChat}>
+    <input type="value" placeholder="username" bind:value={username}>
+    <button>JOIN!</button>
+</form>
+
+<style>
+	h2 {
+		color: blue;
+		font-weight: bold;
+    }
+</style>
